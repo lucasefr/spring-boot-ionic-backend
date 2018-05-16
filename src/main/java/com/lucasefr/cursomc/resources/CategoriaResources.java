dd41b572c9c22c8d8e7b5becae66330330a78ee6
Lucas Emanuel <lucasefr@gmail.com>
2018-05-16 15:55:47 -0300

    Criando repository e service para Categoria 2

--- src/main/java/com/lucasefr/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/lucasefr/cursomc/resources/CategoriaResources.java
@@ -1,8 +1,5 @@
 package com.lucasefr.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +22,6 @@ public class CategoriaResources {
 		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
+			
 	}
 }
